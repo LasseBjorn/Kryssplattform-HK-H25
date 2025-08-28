70c8ef5fda4e481d5687925811bfbabaf32b3d97
lasse <test@mail.com>
2025-08-28 15:02:55 +0200

    modal not finished

--- app/index.tsx
+++ app/index.tsx
@@ -1,19 +1,51 @@
-import { Text, StyleSheet, View } from 'react-native';
+import Post from '@/components/Post';
+import { PostData } from '@/types/post';
+import { Stack } from 'expo-router';
+import { useState } from 'react';
+import { Text, StyleSheet, View, FlatList, Pressable, Modal } from 'react-native';
 
 export default function HomeScreen() {
+
+  const posts: PostData[ ] = [
+    {
+      title: "Mitt første innlegg",
+      description: "Sensasjonelt",
+    },
+    {title: "Mitt andre innlegg",
+      description: "Supert"
+    },
+  ];
+
+  const [isModalVisible, setIsModalVisible] = useState (false);
+
   return (
-  <View>
-    <View style={styles.post}>
-    <Text>Mitt første innlegg</Text>
-    <View>
-      <Text>Helt vanvitting bra</Text>
-    </View>
-    </View>
+  <View style={styles.mainContainer}>
+    <Stack.Screen 
+    options={{
+      headerRight: () => (
+        <Pressable onPress={() => console.log("Kna")}>
+          <Text>Knapp?</Text>
+        </Pressable>
+      ),
+    }}
+    />
+    <Modal visible></Modal>
+    <FlatList
+    data={posts}
+    ItemSeparatorComponent={() => <View style={{height: 12}}></View>}
+    renderItem={(posts) => <Post postData={posts.item}/>}
+    />
   </View>
   );
 }
 
 const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+    paddingHorizontal: 16,
+    paddingTop: 12,
+
+  },
   post: {
     backgroundColor: "white", 
     borderRadius: 10,
